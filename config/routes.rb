6cbd3ae6caa260f80c958ea95947f9f096aac94b
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-07-01 13:57:52 +0000

    routing完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,38 @@
 Rails.application.routes.draw do
   
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/show'
-    get 'comments/destroy'
-  end
-  namespace :admin do
-    get 'recipes/show'
-    get 'recipes/destroy'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'comments/new'
-    get 'comments/create'
-  end
-  namespace :public do
-    get 'favorites/index'
-    get 'favorites/update'
-    get 'favorites/create'
-    get 'favorites/destroy'
-  end
-  namespace :public do
-    get 'recipes/index'
-    get 'recipes/new'
-    get 'recipes/create'
-    get 'recipes/show'
-    get 'recipes/edit'
-    get 'recipes/update'
-    get 'recipes/destroy'
-  end
-  # ユーザー側
+  # ユーザー用
+  # URL /users/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
   
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  
-  # 管理者側
+  # 管理者用
+  # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
   
-  namespace :public do
-    get 'checks/index'
-    get 'checks/new'
-    get 'checks/create'
-    get 'checks/show'
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+  
+  scope module: :public do
+    get 'users/mypage' => 'users#show'
+    get 'users/information/edit' => 'users#edit'
+    patch 'users/mypage' => 'users#update'
+    resources :checks, only: [:index, :new, :create, :show]
+    resources :recipes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :favorites, only: [:index, :update, :create, :destroy]
+    resources :comments, only: [:new, :create]
   end
+  
+  namespace :admin do
+    root to: 'homes#top'
+    resources :recipes, only: [:show, :destroy]
+    resources :comments, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+ 
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
