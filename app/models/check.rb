ecca18ef2a81f1d1cdf20101bd564e4d2d063a9a
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-07-02 00:54:44 +0000

    アソシエーション結んだ

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,2 +1,5 @@
 class Check < ApplicationRecord
+  
+  belongs_to :user
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     resources :recipes, only: [:show, :destroy]
     resources :comments, only: [:index, :show, :destroy]
-    resources :users, only: [:index, :show, :edit, :update]
+    resources :users, only: [:show]
   end
  
   
